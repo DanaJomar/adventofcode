3afe7d093037c11512cc54470b55d2f7dc28bd7b
Dana Jomar <jod@pop-os.localdomain>
2021-12-01 11:22:07 +0100

    prepare and use a template for the solutions

--- 2021/Day1/Solution.py
+++ 2021/Day1/Solution.py
@@ -1,10 +1,22 @@
+##### Day 1 #####
+# https://adventofcode.com/2021/day/1
+
+# ------------------------------------------------
+# imports
+# ------------------------------------------------
 import numpy as np
 
+# ------------------------------------------------
+# read and prepare input
+# ------------------------------------------------
 with open("input.txt") as f:
     lines = f.readlines()
 depths = np.array([int(x.replace("\n", "")) for x in lines])
 
-# ---------------- part 1 -------------------
+# ------------------------------------------------
+# Solution
+# ------------------------------------------------
+# ---------------- part 1 ----------------
 def get_number_of_increases(data_array):
     return (np.roll(data_array, 1)[1:] - data_array[1:] < 0).sum()
 
@@ -13,7 +25,7 @@ def get_number_of_increases(data_array):
 get_number_of_increases(depths)
 ## 1688
 
-# ---------------- part 2 -------------------
+# ---------------- part 2 ----------------
 def rolling_sum(data_array, window_size):
     # https://stackoverflow.com/a/38507725/4905565
     return np.convolve(data_array, np.ones(window_size, dtype=int), "valid")
