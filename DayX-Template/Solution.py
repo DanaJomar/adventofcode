c70b680676a52d28922d381c08e6adb9aecf228e
Dana Jomar <dana.jomar@it-ps.at>
2022-12-15 09:50:21 +0100

    add the possibility to read data with numpy to the template

--- DayX-Template/Solution.py
+++ DayX-Template/Solution.py
@@ -5,12 +5,15 @@
 # imports
 # ------------------------------------------------
 import pandas as pd
+import numpy as np
 
 # ------------------------------------------------
 # read and prepare input
 # ------------------------------------------------
 
 data = pd.read_csv("input.csv", delim_whitespace=True, header=None)
+
+data = np.loadtxt("input_test.csv", dtype=str)
 
 with open("input.csv", "r") as file:
     lines = file.readlines()
